11b990c95c3d4017d60ea214fbffd3b709e51198
Benny24aa <harleyb101010@gmail.com>
2024-12-30 12:21:52 +0000

    Activity Types added based on hours played on roster

--- PD Membership Graphs.R
+++ PD Membership Graphs.R
@@ -30,4 +30,30 @@ PD_Roster_Full_Cleaned <- PD_Roster_Full_Cleaned %>%
   mutate(playtime_2_weeks = gsub("hours","",playtime_2_weeks))|> 
   mutate(playtime_2_weeks = gsub(" ","",playtime_2_weeks))
 
-  
+PD_Roster_Full_Cleaned$playtime_2_weeks <- as.numeric(as.character(PD_Roster_Full_Cleaned$playtime_2_weeks))
+
+PD_Roster_Full_Cleaned <- PD_Roster_Full_Cleaned |>
+  mutate(
+    Activity_Zero = playtime_2_weeks == 0 ,
+    Activity_Bare_Minimum = playtime_2_weeks > 0 & playtime_2_weeks <= 20,
+    Activity_Well = playtime_2_weeks >20 & playtime_2_weeks <=40,
+    Activity_Great = playtime_2_weeks > 40
+  )
+
+#Given Critera to search for when considering activity boundaries
+
+PD_Roster_Full_Cleaned <- PD_Roster_Full_Cleaned |>
+  mutate(Activity_Type = case_when(
+    Activity_Zero == TRUE ~ "Inactive",
+    Activity_Bare_Minimum == TRUE ~ "Needs Improvement",
+    Activity_Well == TRUE ~ "Good",
+    Activity_Great == TRUE ~ "Very Good"
+  ))
+
+### Removes columns related to conditions for activity
+
+PD_Roster_Full_Cleaned <- PD_Roster_Full_Cleaned |>
+  select(-Activity_Zero) |>
+  select(-Activity_Bare_Minimum)|>
+  select(-Activity_Well)|>
+  select(-Activity_Great)
