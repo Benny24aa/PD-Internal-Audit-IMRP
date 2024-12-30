857babf60fdbc8a97c666b542795d05256056c49
Benny24aa <harleyb101010@gmail.com>
2024-12-30 22:24:23 +0000

    Markdown prep for themes for ggplot

--- PD Membership Graphs.R
+++ PD Membership Graphs.R
@@ -98,3 +98,30 @@ Rank_Member_Count <- PD_Roster_Full_Rank_Cleaned %>%
   group_by(source, rank) %>% 
   summarise(count=n(), .groups = 'drop')
 
+
+#### Markdown Prep for Graphs
+
+line_graph_theme <- theme(axis.title = element_text(colour="#3A3776", family = "sans"),
+                       axis.text.x = element_text(size=14, colour = "black"),
+                       axis.text.y = element_text(size=14, colour = "black"),
+                       axis.title.x = element_text(size=18),
+                       axis.title.y = element_text(size=18),
+                       panel.background = element_blank(),
+                       panel.grid.major.y = element_line(colour = "light grey"),
+                       panel.grid.major.x = element_blank(),
+                       axis.line.x = element_line(colour="black"),
+                       axis.line.y = element_line(colour="black"),
+                       plot.margin = margin(.5,1,.5,.5, "cm"))
+
+bar_graph_theme <- theme(axis.title = element_text(colour="#3A3776", family = "sans"),
+                      axis.text.x = element_text(size=14, colour = "black"),
+                      axis.text.y = element_text(size=14, colour = "black"),
+                      axis.title.x = element_text(size=18),
+                      axis.title.y = element_text(size=18),
+                      panel.background = element_blank(),
+                      panel.grid.major.x = element_line(colour = "light grey"),
+                      panel.grid.major.y = element_blank(),
+                      axis.line.x = element_line(colour="black"),
+                      axis.line.y = element_line(colour="black"),
+                      plot.margin = margin(.5,1,.5,.5, "cm"))
+
